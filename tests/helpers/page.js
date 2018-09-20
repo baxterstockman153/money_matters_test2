8b483bc1c6a2b33be7035443fff5b8007dbca489
Mark Huber <uxhuber@gmail.com>
2018-09-20 11:04:43 -0700

    test fixes

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -4,11 +4,11 @@ const userFactory = require("../factories/userFactory");
 
 class CustomPage {
   static async build() {
-    const browser = await puppeteer.launch({ headless: false });
-    // const browser = await puppeteer.launch({
-    //   headless: true,
-    //   args: ["--no-sandbox"]
-    // });
+    // const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"]
+    });
 
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
